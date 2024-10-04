4ac5ca357316d37419e62e26c952617aa9fd835b
salibert81 <salibert81@users.noreply.github.com>
2024-10-04 09:37:42 +0000

    ✅ Ready to clone and code.

--- salfastapi/app.py
+++ salfastapi/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+salfastapi API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="salfastapi",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://salfastapi.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "salibert81",
+        "url": "http://salfastapi.com/contact/",
+        "email": "salibert81@salfastapi.com",
     },
     license_info={
         "name": "The Unlicense",
